6bdecc85c6f93415dd07780f7edaaca6e740d35b
ILya-Lev <lis2889@gmail.com>
2017-02-13 11:58:50 +0200

    properties of IsChanged and OriginalValue family are added to AddresWrapper class

--- Starter/FriendStorage/FriendStorage.UI/Wrappers/AddressWrapper.cs
+++ Starter/FriendStorage/FriendStorage.UI/Wrappers/AddressWrapper.cs
@@ -7,8 +7,19 @@ namespace FriendStorage.UI.Wrappers
 		public AddressWrapper(Address model) : base(model) { }
 
 		public int Id => Model.Id;
+		public int IdOriginalValue => GetOriginalValue<int>(nameof(Id));
+		public bool IdIsChanged => GetIsChanged(nameof(Id));
+
 		public string City { get { return Model.City; } set { SetValue(value); } }
+		public string CityOriginalValue => GetOriginalValue<string>(nameof(City));
+		public bool CityIsChanged => GetIsChanged(nameof(City));
+
 		public string Street { get { return Model.Street; } set { SetValue(value); } }
+		public string StreetOriginalValue => GetOriginalValue<string>(nameof(Street));
+		public bool StreetIsChanged => GetIsChanged(nameof(Street));
+
 		public string Streetnumber { get { return Model.Streetnumber; } set { SetValue(value); } }
+		public string StreetnumberOriginalValue => GetOriginalValue<string>(nameof(Streetnumber));
+		public bool StreetnumberIsChanged => GetIsChanged(nameof(Streetnumber));
 	}
 }
---
+++ Starter/FriendStorage/FriendStorage.UIxUnitTests/Wrappers/ChangeTrackingComplexPropertiesTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using FriendStorage.Model;
+using FriendStorage.UI.Wrappers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FriendStorage.UIxUnitTests.Wrappers
+{
+	public class ChangeTrackingComplexPropertiesTests
+	{
+		private FriendWrapper _wrapper;
+		private Friend _expectedFriend;
+
+		public ChangeTrackingComplexPropertiesTests()
+		{
+			_expectedFriend = new Friend
+			{
+				FirstName = "Alan",
+				LastName = "Johns",
+				Address = new Address { City = "Munich" },
+				Emails = new List<FriendEmail>()
+			};
+
+			_wrapper = new FriendWrapper(_expectedFriend);
+		}
+
+		[Fact]
+		public void City_Changed_ShouldMarkFriendWrapperAsChanged()
+		{
+			_wrapper.IsChanged.Should().BeFalse("no changes yet");
+
+			_wrapper.Address.City = "London";
+
+			_wrapper.IsChanged.Should().BeTrue($"city is changed from {_wrapper.Address.CityOriginalValue} to {_wrapper.Address.City}");
+
+			_wrapper.Address.City = "Munich";
+			_wrapper.IsChanged.Should().BeFalse($"city value is restored to {_wrapper.Address.City} - its original value");
+		}
+	}
+}
