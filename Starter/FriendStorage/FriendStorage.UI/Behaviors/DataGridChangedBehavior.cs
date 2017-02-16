01b3b2881a8f7316647abe4dad6b4714f6248a7c
ILya-Lev <lis2889@gmail.com>
2017-02-16 19:48:06 +0200

    should work but cannot find datagridchangedbehavior

---
+++ Starter/FriendStorage/FriendStorage.UI/Behaviors/DataGridChangedBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FriendStorage.UI.Behaviors
+{
+	public static class DataGridChangedBehavior
+	{
+		public static DependencyProperty IsActiveProperty { get; }
+
+		static DataGridChangedBehavior()
+		{
+			IsActiveProperty = DependencyProperty.Register("IsActive",
+								typeof(bool), typeof(DataGridChangedBehavior),
+								new PropertyMetadata(default(bool), OnIsActivePropertyChanged));
+		}
+
+		public static bool GetIsActive(DependencyObject obj)
+		{
+			return (bool) obj.GetValue(IsActiveProperty);
+		}
+
+		public static void SetValue(DependencyObject obj, bool value)
+		{
+			obj.SetValue(IsActiveProperty, value);
+		}
+		private static void OnIsActivePropertyChanged(DependencyObject d,
+													DependencyPropertyChangedEventArgs e)
+		{
+			var dataGrid = d as DataGrid;
+			if (dataGrid == null) return;
+
+			if ((bool) e.NewValue) dataGrid.Loaded += DataGrid_Loaded;
+			else dataGrid.Loaded -= DataGrid_Loaded;
+		}
+
+		private static void DataGrid_Loaded(object sender, RoutedEventArgs e)
+		{
+			var dataGrid = sender as DataGrid;
+			foreach (var textColumn in dataGrid.Columns.OfType<DataGridTextColumn>())
+			{
+				var binding = textColumn.Binding as Binding;
+				if (binding == null) continue;
+
+				textColumn.EditingElementStyle = CreateEditingElementStyle(dataGrid, binding.Path.Path);
+				textColumn.ElementStyle = CreateElementStyle(dataGrid, binding.Path.Path);
+			}
+		}
+
+		private static Style CreateElementStyle(DataGrid dataGrid, string path)
+		{
+			var baseStyle = dataGrid.FindResource("TextBlockBaseStyle") as Style;
+			var style = new Style(typeof(TextBox), baseStyle);
+			AddSetters(style, path);
+			return style;
+		}
+
+		private static Style CreateEditingElementStyle(DataGrid dataGrid, string path)
+		{
+			var baseStyle = dataGrid.FindResource(typeof(TextBox)) as Style;
+			var style = new Style(typeof(TextBox), baseStyle);
+			AddSetters(style, path);
+			return style;
+		}
+
+		private static void AddSetters(Style style, string path)
+		{
+			style.Setters.Add(new Setter(ChangedBehavior.IsActiveProperty, false));
+			style.Setters.Add(new Setter(ChangedBehavior.IsChangedProperty,
+								new Binding($"{path}IsChanged")));
+			style.Setters.Add(new Setter(ChangedBehavior.OriginalValueProperty,
+								new Binding($"{path}OriginalValue")));
+		}
+	}
+}
